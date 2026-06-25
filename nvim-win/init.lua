85659c3eb763938f6de26ffe263e56314a6fc677
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2026-06-25 17:55:00 -0300

    .

--- nvim-win/init.lua
+++ nvim-win/init.lua
@@ -1,53 +1,75 @@
+
+-- ============================================================
+-- CORE
+-- ============================================================
 vim.loader.enable()
 vim.g.mapleader = " "
+
+-- clipboard Windows
+vim.opt.clipboard = "unnamedplus"
+
+-- shell Windows safe
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+elseif vim.fn.executable("powershell") == 1 then
+  vim.o.shell = "powershell"
+end
 
 -- ============================================================
 -- LAZY BOOTSTRAP
 -- ============================================================
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
+    "--branch=stable",
+    lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 -- ============================================================
 -- PLUGINS
 -- ============================================================
-
 require("lazy").setup({
+
   -- THEME
   { "folke/tokyonight.nvim" },
 
-  -- PRODUCTIVITY
+  -- UI
   { "nvim-tree/nvim-web-devicons" },
   { "folke/which-key.nvim", opts = {} },
 
+  -- AUTOPAIRS
   { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
 
+  -- FILE EXPLORER
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = { view_options = { show_hidden = true } },
   },
 
+  -- TELESCOPE
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
 
+  -- GIT
   { "lewis6991/gitsigns.nvim", opts = {} },
 
-  -- LSP + MASON
-  { "williamboman/mason.nvim" },
+  -- MASON
+  { "williamboman/mason.nvim", opts = {} },
   { "williamboman/mason-lspconfig.nvim" },
+
+  -- LSP CONFIG (registry)
   { "neovim/nvim-lspconfig" },
 
-  -- AUTOCOMPLETE
+  -- CMP
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -59,16 +81,16 @@ require("lazy").setup({
     },
   },
 
-  -- TREESITTER (CORRIGIDO)
+  -- TREESITTER
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
       ensure_installed = {
         "lua",
+        "vim",
+        "vimdoc",
         "javascript",
         "typescript",
         "html",
@@ -81,59 +103,61 @@ require("lazy").setup({
         "cpp",
         "php",
       },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
   },
 })
 
 -- ============================================================
 -- OPTIONS
 -- ============================================================
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = "a"
-vim.o.clipboard = "unnamedplus"
-vim.o.shell = "pwsh"
 vim.o.termguicolors = true
+vim.o.signcolumn = "yes"
 
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.smartindent = true
 
-vim.o.scrolloff = 8
-vim.o.signcolumn = "yes"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 200
+
 vim.o.splitbelow = true
 vim.o.splitright = true
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 250
-
-vim.o.autochdir = false
+vim.o.scrolloff = 8
 
 -- ============================================================
 -- THEME
 -- ============================================================
-
 vim.cmd.colorscheme("tokyonight-night")
 
 -- ============================================================
--- KEYMAPS BASE
+-- KEYMAPS
 -- ============================================================
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil" })
+local telescope = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
-vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
+vim.keymap.set("n", "-", "<cmd>Oil<cr>")
 
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
+
+-- clipboard
+vim.keymap.set({ "n", "v" }, "<C-c>", '"+y')
+vim.keymap.set("v", "<C-v>", '"+p')
+vim.keymap.set("i", "<C-v>", "<C-r>+")
 
 -- terminal
 vim.keymap.set("n", "<leader>t", function()
@@ -142,69 +166,16 @@ vim.keymap.set("n", "<leader>t", function()
   vim.cmd("startinsert")
 end)
 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
--- window navigation
+-- navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
--- reload config
-vim.keymap.set("n", "<leader>r", "<cmd>source $MYVIMRC<CR>")
-
 -- ============================================================
--- AUTOPAIRS
--- ============================================================
-
-require("nvim-autopairs").setup()
-
--- ============================================================
--- MASON
--- ============================================================
-
-require("mason").setup()
-
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "ts_ls",
-    "html",
-    "cssls",
-    "jsonls",
-    "pyright",
-    "gopls",
-    "rust_analyzer",
-    "intelephense",
-    "clangd",
-  },
-})
-
--- ============================================================
--- LSP (NEOVIM 0.11+ CORRIGIDO)
--- ============================================================
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-vim.lsp.config("*", {
-  capabilities = capabilities,
-})
-
-vim.lsp.enable({
-  "lua_ls",
-  "ts_ls",
-  "html",
-  "cssls",
-  "jsonls",
-  "pyright",
-  "gopls",
-  "rust_analyzer",
-  "intelephense",
-  "clangd",
-})
-
--- ============================================================
--- CMP
+-- AUTOPAIRS + CMP
 -- ============================================================
 
 local cmp = require("cmp")
@@ -231,13 +202,51 @@ cmp.setup({
   },
 })
 
--- autopairs + cmp
-require("nvim-autopairs").setup()
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 -- ============================================================
--- FINAL
+-- MASON + LSP (NOVO NVIM 0.12)
 -- ============================================================
 
-print("Neovim loaded successfully")
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "ts_ls",
+    "html",
+    "cssls",
+    "jsonls",
+    "pyright",
+    "gopls",
+    "rust_analyzer",
+    "intelephense",
+    "clangd",
+  },
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- NOVO SISTEMA LSP (SEM lspconfig.setup)
+vim.lsp.config("*", {
+  capabilities = capabilities,
+})
+
+vim.lsp.enable({
+  "lua_ls",
+  "ts_ls",
+  "html",
+  "cssls",
+  "jsonls",
+  "pyright",
+  "gopls",
+  "rust_analyzer",
+  "intelephense",
+  "clangd",
+})
+
+-- ============================================================
+-- FINAL
+-- ============================================================
+print("Neovim OK")
