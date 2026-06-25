f09167c1eaedaf95319e93d88fc8cef774a339b9
Caio Seabra de Queiroz <144072410+caioxdev@users.noreply.github.com>
2026-06-25 18:05:58 -0300

    .

--- nvim-win/init.lua
+++ nvim-win/init.lua
@@ -1,22 +1,54 @@
-
 -- ============================================================
 -- CORE
 -- ============================================================
 vim.loader.enable()
 vim.g.mapleader = " "
 
--- clipboard Windows
 vim.opt.clipboard = "unnamedplus"
-
--- shell Windows safe
-if vim.fn.executable("pwsh") == 1 then
-  vim.o.shell = "pwsh"
-elseif vim.fn.executable("powershell") == 1 then
-  vim.o.shell = "powershell"
-end
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 150
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 8
 
 -- ============================================================
--- LAZY BOOTSTRAP
+-- DIAGNOSTICS
+-- ============================================================
+vim.diagnostic.config({
+  virtual_text = { prefix = "●" },
+  float = { border = "rounded" },
+  severity_sort = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
+-- ============================================================
+-- FLOAT UI (moderno sem deprecated)
+-- ============================================================
+local function border(handler)
+  return function(err, result, ctx, config)
+    config = config or {}
+    config.border = "rounded"
+    return handler(err, result, ctx, config)
+  end
+end
+
+vim.lsp.handlers["textDocument/hover"] = border(vim.lsp.handlers.hover)
+vim.lsp.handlers["textDocument/signatureHelp"] = border(vim.lsp.handlers.signature_help)
+
+-- ============================================================
+-- LAZY
 -- ============================================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -32,46 +64,51 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- ============================================================
--- PLUGINS
+-- PLUGINS (LAZY BY DESIGN)
 -- ============================================================
 require("lazy").setup({
 
-  -- THEME
   { "folke/tokyonight.nvim" },
-
-  -- UI
   { "nvim-tree/nvim-web-devicons" },
-  { "folke/which-key.nvim", opts = {} },
+  { "folke/which-key.nvim", opts = {}, event = "VeryLazy" },
 
-  -- AUTOPAIRS
-  { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
-
-  -- FILE EXPLORER
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Oil",
     opts = { view_options = { show_hidden = true } },
   },
 
-  -- TELESCOPE
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
   },
 
-  -- GIT
-  { "lewis6991/gitsigns.nvim", opts = {} },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+
+  { "lewis6991/gitsigns.nvim", event = "BufReadPost", opts = {} },
 
   -- MASON
-  { "williamboman/mason.nvim", opts = {} },
-  { "williamboman/mason-lspconfig.nvim" },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    opts = {},
+  },
 
-  -- LSP CONFIG (registry)
-  { "neovim/nvim-lspconfig" },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = "BufReadPre",
+  },
 
   -- CMP
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
@@ -88,49 +125,15 @@ require("lazy").setup({
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "json",
-        "python",
-        "go",
-        "rust",
-        "c",
-        "cpp",
-        "php",
+        "lua","vim","vimdoc",
+        "js","ts","html","css","json",
+        "python","go","rust","c","cpp","php",
       },
       highlight = { enable = true },
       indent = { enable = true },
     },
   },
 })
-
--- ============================================================
--- OPTIONS
--- ============================================================
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.mouse = "a"
-vim.o.termguicolors = true
-vim.o.signcolumn = "yes"
-
-vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.smartindent = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 200
-
-vim.o.splitbelow = true
-vim.o.splitright = true
-
-vim.o.scrolloff = 8
 
 -- ============================================================
 -- THEME
@@ -140,11 +143,9 @@ vim.cmd.colorscheme("tokyonight-night")
 -- ============================================================
 -- KEYMAPS
 -- ============================================================
-
 local telescope = require("telescope.builtin")
 
 vim.keymap.set("n", "-", "<cmd>Oil<cr>")
-
 vim.keymap.set("n", "<leader>ff", telescope.find_files)
 vim.keymap.set("n", "<leader>fg", telescope.live_grep)
 vim.keymap.set("n", "<leader>fb", telescope.buffers)
@@ -152,43 +153,27 @@ vim.keymap.set("n", "<leader>fb", telescope.buffers)
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
 
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
-
--- clipboard
-vim.keymap.set({ "n", "v" }, "<C-c>", '"+y')
-vim.keymap.set("v", "<C-v>", '"+p')
-vim.keymap.set("i", "<C-v>", "<C-r>+")
-
--- terminal
-vim.keymap.set("n", "<leader>t", function()
-  vim.cmd("botright 12split")
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
-end)
-
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-
--- navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
--- ============================================================
--- AUTOPAIRS + CMP
--- ============================================================
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
 
+-- ============================================================
+-- CMP
+-- ============================================================
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
 
   mapping = cmp.mapping.preset.insert({
-    ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
@@ -202,13 +187,12 @@ cmp.setup({
   },
 })
 
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+-- autopairs
+require("nvim-autopairs").setup()
 
 -- ============================================================
--- MASON + LSP (NOVO NVIM 0.12)
+-- MASON
 -- ============================================================
-
 require("mason").setup()
 
 require("mason-lspconfig").setup({
@@ -221,32 +205,75 @@ require("mason-lspconfig").setup({
     "pyright",
     "gopls",
     "rust_analyzer",
-    "intelephense",
     "clangd",
   },
 })
 
+-- ============================================================
+-- LSP CAPABILITIES
+-- ============================================================
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- NOVO SISTEMA LSP (SEM lspconfig.setup)
-vim.lsp.config("*", {
-  capabilities = capabilities,
-})
+-- ============================================================
+-- FORMAT BY LANGUAGE (IMPORTANTE)
+-- ============================================================
+local format_map = {
+  lua = function()
+    return { "stylua" }
+  end,
+  python = function()
+    return { "black" }
+  end,
+  javascript = function()
+    return { "prettier" }
+  end,
+  typescript = function()
+    return { "prettier" }
+  end,
+}
 
-vim.lsp.enable({
-  "lua_ls",
-  "ts_ls",
-  "html",
-  "cssls",
-  "jsonls",
-  "pyright",
-  "gopls",
-  "rust_analyzer",
-  "intelephense",
-  "clangd",
-})
+local function format_on_save(bufnr)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr })
+    end,
+  })
+end
+
+-- ============================================================
+-- LSP PER FILETYPE (ESSENCIAL)
+-- ============================================================
+local function setup_lsp(name, config)
+  vim.lsp.config(name, {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr }
+
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      format_on_save(bufnr)
+    end,
+  })
+
+  vim.lsp.enable(name)
+end
+
+setup_lsp("lua_ls")
+setup_lsp("ts_ls")
+setup_lsp("html")
+setup_lsp("cssls")
+setup_lsp("jsonls")
+setup_lsp("pyright")
+setup_lsp("gopls")
+setup_lsp("rust_analyzer")
+setup_lsp("clangd")
 
 -- ============================================================
 -- FINAL
 -- ============================================================
-print("Neovim OK")
+print("Neovim ARCH READY (optimized + lazy + modular LSP)")
